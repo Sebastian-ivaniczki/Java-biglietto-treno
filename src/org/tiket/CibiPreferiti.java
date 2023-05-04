582e48688d15c49187d4f509476dd50148cc9df1
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-05-04 15:47:46 +0200

    fix

--- src/org/tiket/CibiPreferiti.java
+++ src/org/tiket/CibiPreferiti.java
@@ -6,7 +6,11 @@ public class CibiPreferiti {
 		
 		System.out.println("lunghezza: " + preferiteFood.length);
 		System.out.println("primo piatto: " + preferiteFood[0]);
-		System.out.println("ultimo piatto: " + preferiteFood[4]);
-		System.out.println("piatto intermedio: " + preferiteFood[2]);
+		System.out.println("ultimo piatto: " + preferiteFood[preferiteFood.length -1]);
+		
+		if(preferiteFood.length % 2 != 0) {
+			
+			System.out.println("piatto intermedio: " + preferiteFood[preferiteFood.length /2]);
+		}
 	}
 }
